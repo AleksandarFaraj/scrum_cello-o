f20491425d62140cda790369002f56991bf11b02
Gabriel Alpsten <galpsten@hotmail.com>
2014-08-16 14:06:30 +0200

    Diet info page

---
+++ diet.php
@@ -0,0 +1,16 @@
+<?php include('partials/header.php'); ?>
+	<section class="main container">
+		<div class="row">
+			<div class="container">
+				<h1>Diet Information</h1>
+			</div>
+			<div class="col-lg-8">
+				<p>
+					Here goes text...
+				</p>
+			</div>
+				
+		</div>
+		
+	</section>
+<?php include('partials/footer.php'); ?>
