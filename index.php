0eb16c4457c81798a10dc8a9e6f4688c3b2c01f0
rippedspine <gabriella.karin@gmail.com>
2014-08-16 13:58:24 +0200

    add index

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php include('partials/header.php'); ?>
+	<section class="main container">
+		<div class="row">
+			<div class="container">
+				<h1>Cell-o</h1>
+			</div>
+			<div class="col-lg-4">
+				<img src="img/cello.jpg" alt="">
+			</div>
+			<div class="col-lg-8">
+				<p>
+					Välkommen till min informationssida om Cell-O!
+					Människosläktet definierat Cell-O som ett djur.
+					Cell-O är en missförstådd entitet, “det” flyger runt bland mörk materia och 
+					letar efter en vän men med sin kraft förstör “det” solsystem utan att veta om 
+					det själv.
+					Vi har skapat denna sida för att vi vill hjälpa Cell-O att hitta en vän.
+				</p>
+			</div>
+				
+		</div>
+		
+	</section>
+<?php include('partials/footer.php'); ?>
---
+++ partials/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Cell-o</title>
+	<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+	<header>
+	<nav class="navbar navbar-default" role="navigation">
+	  <div class="container-fluid">
+	    <!-- Brand and toggle get grouped for better mobile display -->
+	    <div class="navbar-header">
+	      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+	        <span class="sr-only">Toggle navigation</span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	      </button>
+	      <a class="navbar-brand" href="/">Cell-o</a>
+	    </div>
+
+	    <!-- Collect the nav links, forms, and other content for toggling -->
+	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+	      <ul class="nav navbar-nav">
+	        <li class="active"><a href="/">Home</a></li>
+	      </ul>
+	    </div><!-- /.navbar-collapse -->
+	  </div><!-- /.container-fluid -->
+	</nav>
+	</header>
