df93b19679d430ec302bddfe7b43e414ad265bba
Gabriel Alpsten <galpsten@hotmail.com>
2014-08-17 14:58:28 +0200

    Revert "temp"
    This reverts commit c55a6c635573614f651c1a8638015438e1363791.

--- communication.php
+++ communication.php
@@ -8,7 +8,7 @@
 				<h2>Kommunicerar den med andra av sig egen art?</h2>
 				<p>Den brukar förekomma samtal med ljud, men bara under erfermiddagar.</p>
 				<h2>Pratar den med andra om sin art?</h2>
-				<p>Nej, eftersom Cell-O luktar så illa så vill inte någon prata mkomma nära. Cell-O är inte se bekant med teknik, så Skype är inte ett alternativ heller.</p>
+				<p>Nej, eftersom Cell-O luktar så illa så vill inte någon prata mkomma nära. Cell-O är inte se bekand med teknik, så Skype är inte ett alternativ heller.</p>
 
 			</div>
 				
