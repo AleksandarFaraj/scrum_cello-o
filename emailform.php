00c8872b3a90b833ad80d8cbd3e1fb182a43777f
Gabriel Alpsten <galpsten@hotmail.com>
2014-08-17 14:57:28 +0200

    emailform

--- emailform.php
+++ emailform.php
@@ -1,16 +1,33 @@
-<?php include('partials/header.php'); ?>
+<?php 
+include('partials/header.php'); 
+include('partials/mail.php'); 
+?>
+
 	<section class="main container">
 		<div class="row">
 			<div class="container">
 				<h1>Mail</h1>
 			</div>
-			<div class="col-lg-8">
-				<form>
-				From: <input name="sender" type="text" /> <br />
-				Subject: <input name="subject" type="text" /> <br />
-				Message: <input name="message" type="text" />
+			<div class="col-lg-6">
+				<form role="form" method="post">
+				
+				<div class="form-group">
+					<label for="sender">Från</label>
+						<input name="sender" type="text" class="form-control"/> 
+				</div>
+			
+				<div class="form-group">
+					<label for="subject">Titel</label>
+						<input name="subject" type="text" class="form-control"/> 
+				</div>
+				
+				<div class="form-group ">
+					<label for="message">Meddelande</label>
+						<textarea name="message" class="form-control" ></textarea>
+				</div>
 				
 				
+				<input type="submit" />
 				</form>
 				
 			</div>
