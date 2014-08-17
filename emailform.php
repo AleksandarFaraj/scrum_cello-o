e2b6c466abd8c03e54c6c820d3dc69c0835935ed
Gabriel Alpsten <galpsten@hotmail.com>
2014-08-17 14:40:18 +0200

    emailform

---
+++ emailform.php
@@ -0,0 +1,21 @@
+<?php include('partials/header.php'); ?>
+	<section class="main container">
+		<div class="row">
+			<div class="container">
+				<h1>Mail</h1>
+			</div>
+			<div class="col-lg-8">
+				<form>
+				From: <input name="sender" type="text" /> <br />
+				Subject: <input name="subject" type="text" /> <br />
+				Message: <input name="message" type="text" />
+				
+				
+				</form>
+				
+			</div>
+				
+		</div>
+		
+	</section>
+<?php include('partials/footer.php'); ?>
