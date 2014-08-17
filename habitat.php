5b365609a0154a1704e6fb0e4e22d6cf3b1c743d
rippedspine <gabriella.karin@gmail.com>
2014-08-17 14:54:06 +0200

    add hab

---
+++ habitat.php
@@ -0,0 +1,16 @@
+<?php include('partials/header.php'); ?>
+	<section class="main container">
+		<div class="row">
+			<div class="container">
+				<h1>Bostad</h1>
+			</div>
+			<div class="col-lg-8">
+			<p>
+				Cello bygger ofta sitt näste i hörn där det är nära till föda. Det har hänt att cello har setts  i storstäder, men det är väldigt sällan.</p>				
+			</p>
+			</div>
+				
+		</div>
+		
+	</section>
+<?php include('partials/footer.php'); ?>
